9e9ac3c26be9d56983c59b6e8e4c40a3e0d7a682
abderrahman-teta <abdou.teta95@gmail.com>
2019-02-13 22:04:08 +0100

    model teacher

--- api/models/teacher.js
+++ api/models/teacher.js
@@ -4,7 +4,8 @@ const teacherSchema = new mongoose.Schema({
     user_name:String,
     First_name:String,
     Last_name:String,
-    job:String
+    job:String,
+    gender:String
 }) 
 
 const teacherSch = module.exports = mongoose.model('teacherSch',teacherSchema)
