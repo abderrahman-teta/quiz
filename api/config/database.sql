4a30bfe9615aea887032f846e2f52abbdc7b29dd
abderrahman-teta <abdou.teta95@gmail.com>
2019-02-18 23:23:13 +0100

    add database sql

---
+++ api/config/database.sql
@@ -0,0 +1,17 @@
+
+DROP DATABASE memoire;
+CREATE DATABASE memoire;
+USE memoire;
+
+-- create student table
+CREATE TABLE student (
+ id INT NOT NULL,
+first_name VARCHAR(55),
+last_name VARCHAR(55),
+email VARCHAR(55),
+birthday VARCHAR(55),
+gender VARCHAR(55),
+password_ VARCHAR(55),
+user_name_ VARCHAR(55),
+PRIMARY KEY(id,user_name_)
+);
