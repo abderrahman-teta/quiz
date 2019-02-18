590c49f9a9e52c68120ef853a2962122653f75a8
abderrahman-teta <abderrahman.teta95@gmail.com>
2019-02-18 23:31:09 +0100

    add teacher schema

--- api/config/database.sql
+++ api/config/database.sql
@@ -15,3 +15,16 @@ password_ VARCHAR(55),
 user_name_ VARCHAR(55),
 PRIMARY KEY(id,user_name_)
 );
+-- create teacher table
+CREATE TABLE teacher (
+    id INT NOT NULL,
+first_name VARCHAR(55),
+last_name VARCHAR(55),
+email VARCHAR(55),
+birthday VARCHAR(55),
+gender VARCHAR(55),
+password_ VARCHAR(55),
+user_name_ VARCHAR(55),
+PRIMARY KEY(id,user_name_)
+);
+
