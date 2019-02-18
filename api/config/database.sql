a0e3dbb88aae8ca13349204108ac9a32816dbf22
abdouteta <abdou.teta95@gmail.com>
2019-02-18 23:33:46 +0100

    tacher schema

--- api/config/database.sql
+++ api/config/database.sql
@@ -5,26 +5,27 @@ USE memoire;
 
 -- create student table
 CREATE TABLE student (
- id INT NOT NULL,
-first_name VARCHAR(55),
-last_name VARCHAR(55),
-email VARCHAR(55),
-birthday VARCHAR(55),
-gender VARCHAR(55),
-password_ VARCHAR(55),
-user_name_ VARCHAR(55),
+    id INT NOT NULL,
+    first_name VARCHAR(55),
+    last_name VARCHAR(55),
+    email VARCHAR(55),
+    birthday VARCHAR(55),
+    gender VARCHAR(55),
+    password_ VARCHAR(55),
+    user_name_ VARCHAR(55),
 PRIMARY KEY(id,user_name_)
 );
 -- create teacher table
 CREATE TABLE teacher (
     id INT NOT NULL,
-first_name VARCHAR(55),
-last_name VARCHAR(55),
-email VARCHAR(55),
-birthday VARCHAR(55),
-gender VARCHAR(55),
-password_ VARCHAR(55),
-user_name_ VARCHAR(55),
-PRIMARY KEY(id,user_name_)
+    first_name VARCHAR(55),
+    last_name VARCHAR(55),
+    email VARCHAR(55),
+    birthday VARCHAR(55),
+    gender VARCHAR(55),
+    password_ VARCHAR(55),
+    user_name_ VARCHAR(55),
+    job VARCHAR(55),
+    PRIMARY KEY(id,user_name_)
 );
 
