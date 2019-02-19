4bb78d1b2f47b23e6ccd515bcfc63ac78adb20a1
abderrahman-teta <abderrahman.teta95@gmail.com>
2019-02-19 02:15:01 +0100

    create teacher login function

--- api/teacher/teacher.controler.js
+++ api/teacher/teacher.controler.js
@@ -1,5 +1,25 @@
 
 
+const login  = (req,res) => {
+    prams = {
+       user_name : req.body.user_name,
+       Fisrt_name : req.body.Fisrt_name,
+       Last_name : req.body.Last_name,
+       job : req.body.job
+   }
+    teacherSch.create(prams,(err,teacher)=>{
+       if(!err){
+           res.json({
+               data:teacher
+           })
+       }else{
+           res.json({
+               err:err
+           })
+       }
+   })
+
+}
 const signup = (req,res) => {
      prams = {
         user_name : req.body.user_name,
