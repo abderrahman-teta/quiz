59f646c309d95a9096a3c83f9150b51b145e571e
abderrahman-teta <abderrahman.teta95@gmail.com>
2019-02-19 02:17:30 +0100

    teacher controller add function

--- api/teacher/teacher.controler.js
+++ api/teacher/teacher.controler.js
@@ -1,23 +1,27 @@
 
 
 const login  = (req,res) => {
-    prams = {
-       user_name : req.body.user_name,
-       Fisrt_name : req.body.Fisrt_name,
-       Last_name : req.body.Last_name,
-       job : req.body.job
-   }
-    teacherSch.create(prams,(err,teacher)=>{
-       if(!err){
-           res.json({
-               data:teacher
-           })
-       }else{
-           res.json({
-               err:err
-           })
-       }
-   })
+    const user_name = req.body.user_name_
+    const password = req.body.password_
+   
+    if (user_name && password) {
+		connection.query('SELECT * FROM student WHERE user_name_ = ? AND password_ = ?', [user_name, password], function(error, results, fields) {
+			if (results.length > 0) {
+				res.json({
+                    result: 'welcome to your profile',
+                });
+			} else {
+				res.json({
+                    err: 'Incorrect Username and/or Password!'
+                    
+                });
+			}			
+			res.end();
+		});
+	} else {
+		res.json('Please enter Username and Password!');
+		res.end();
+	}
 
 }
 const signup = (req,res) => {
