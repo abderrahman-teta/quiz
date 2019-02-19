b3da3d414723598231ad25236c687bd577bf244c
abderrahman-teta <abderrahman.teta95@gmail.com>
2019-02-19 02:35:18 +0100

    add login function to teacher controller

--- api/teacher/teacher.controller.js
+++ api/teacher/teacher.controller.js
@@ -25,25 +25,34 @@ const login  = (req,res) => {
 	}
 
 }
-const signup = (req,res) => {
-     prams = {
-        user_name : req.body.user_name,
-        Fisrt_name : req.body.Fisrt_name,
-        Last_name : req.body.Last_name,
-        job : req.body.job
+// signup function
+const signup = (req,res) =>{
+
+    params = [
+        req.body.first_name,
+        req.body.last_name,
+        req.body.email,
+        req.body.birthday,
+        req.body.gender,
+        req.body.password_,
+        req.body.user_name_,
+        req.body.password_2
+    ]
+// match password
+//dfgf
+    if(params[5] == params[7]){
+        connection.query(`INSERT INTO student VALUES('',?,?,?,?,?,?,?)`,params, function (err, result, fields) {
+            if (err) throw err;
+            res.json({
+                result : result
+             });
+          });
+    }else{
+        res.json({
+            data: "passwords not match"
+        })
     }
-     teacherSch.create(prams,(err,teacher)=>{
-        if(!err){
-            res.json({
-                data:teacher
-            })
-        }else{
-            res.json({
-                err:err
-            })
-        }
-    })
-
+    
+    
 }
-
 module.exports = {signup,login}
