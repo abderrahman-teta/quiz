be1bb7cc73791f4142916da8a842f1079a3cde92
abderrahman-teta <abderrahman.teta95@gmail.com>
2019-03-21 18:16:20 +0100

    add login funchtion add change signup fuc

--- app/teacher/teacher.controller.js
+++ app/teacher/teacher.controller.js
@@ -1,21 +1,43 @@
 const Teacher = require('../models/teacher')
 
 const signup = (req,res) => {
-     prams = {
-        user_name : req.body.user_name,
-        Fisrt_name : req.body.Fisrt_name,
-        Last_name : req.body.Last_name,
-        job : req.body.job,
-        
+
+    if(req.body.password == req.body.cpassword) {
+        let teacher = new Teacher({
+            First_name : req.body.fname,
+            Last_name : req.body.lname,
+            email : req.body.email,
+            job : req.body.job,
+            password : req.body.password,
+        })
+        teacher
+            .save()
+            .then(result =>{
+                res.redirect('/login')
+            })
+            .catch(err =>{
+                res.redirect('/signup')
+            })
     }
-    Teacher.create(prams,(err,teacher)=>{ 
-         if(!err){
-          res.json( {data:teacher} ) 
-         }else{
-          res.json({ err:err }) 
-         }
-          
-})
+    else{
+     res.redirect('/signup') 
+}
 }
 
-module.exports = {signup}
+const login = (req,res)=>{
+    let email = req.body.email
+
+    Teacher.find()
+           .then(teacher =>{
+            if(teacher[0].password == req.body.password){
+                res.redirect('/dashboard')
+            }else{
+                 res.render('login')
+            }
+           })
+           .catch(err=>{
+                res.redirect('/login')
+           })
+}
+
+module.exports = {signup,login}
