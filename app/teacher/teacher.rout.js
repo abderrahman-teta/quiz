8e0534934bce631a52aa373c824a1af60e93feaa
abderrahman-teta <abderrahman.teta95@gmail.com>
2019-03-21 18:17:46 +0100

    i dont know what i did here

--- app/teacher/teacher.rout.js
+++ app/teacher/teacher.rout.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const teacher = express.Router()
 
-const {signup} = require('./teacher.controller')
+const {signup,login} = require('./teacher.controller')
 
 teacher.post('/signup',signup)
+
+teacher.post('/login',login)
 
 module.exports = teacher
